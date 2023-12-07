d280e777bbfcd3087e179e348c458063b1f3bb43
Xws-HuiXiang <793061444@qq.com>
2023-12-07 15:26:09 +0800

    增加正弦函数
    增加正弦函数表
    实现正弦函数
    实现钳制函数Clamp

--- FMath/Math/FMath.cs
+++ FMath/Math/FMath.cs
@@ -10,6 +10,35 @@ namespace FixedMath
     public static class FMath
     {
         /// <summary>
+        /// π
+        /// </summary>
+        public readonly static FFloat PI = new FFloat(Math.PI);
+        /// <summary>
+        /// π对应的角度值
+        /// </summary>
+        public const int PIAngle = 360;
+        /// <summary>
+        /// 2π
+        /// </summary>
+        public readonly static FFloat PI2 = new FFloat(2 * Math.PI);
+        /// <summary>
+        /// 2π对应的角度值
+        /// </summary>
+        public const int PI2Angle = 360;
+        /// <summary>
+        /// 自然对数基数 e
+        /// </summary>
+        public readonly static FFloat E = new FFloat(Math.E);
+        /// <summary>
+        /// 弧度转角度的常量：180/π
+        /// </summary>
+        public readonly static FFloat Rad2Deg = 180 / FMath.PI;
+        /// <summary>
+        /// 角度转弧度的常量：π/180
+        /// </summary>
+        public readonly static FFloat Deg2Rad = FMath.PI / 180;
+
+        /// <summary>
         /// 返回指定数字的平方根
         /// </summary>
         /// <param name="value">需要开方的值</param>
@@ -37,6 +66,53 @@ namespace FixedMath
         }
 
         /// <summary>
+        /// 正弦函数
+        /// </summary>
+        /// <param name="radian">弧度值</param>
+        /// <returns></returns>
+        public static FFloat Sin(FFloat radian)
+        {
+            //处理弧度值在0~2PI范围内
+            FFloat value = radian / PI2;
+            radian -= PI2 * value.Int;
+            //处理负值
+            if (radian < 0)
+                radian += PI2;
+            //将 radian 重映射到[0 ~ TotalCount]作为数组索引
+            FFloat index = (radian / PI2) * FMathTable.SinTable.Length;
+            //钳制索引在数组范围内
+            index = FMath.Clamp(index, 0, FMathTable.SinTable.Length - 1);
+            FFloat v = FMathTable.SinTable[index.RoundToInt];
+
+            v /= FMathTable.SCALE;
+
+            return v;
+        }
+
+        /// <summary>
+        /// 正弦函数
+        /// </summary>
+        /// <param name="angle">角度值</param>
+        /// <returns></returns>
+        public static FFloat SinAngle(FFloat angle)
+        {
+            //处理角度在0~360度之间
+            //需要优先处理，因为定点数计算数字越大误差越大
+            if (angle > 0)
+            {
+                while (angle >= FMath.PI2Angle)
+                    angle -= FMath.PI2Angle;
+            }
+            else if (angle < 0)
+            {
+                while (angle <= FMath.PI2Angle)
+                    angle += FMath.PI2Angle;
+            }
+
+            return Sin(angle * FMath.Deg2Rad);
+        }
+
+        /// <summary>
         /// 反余弦函数
         /// </summary>
         /// <param name="angle"></param>
@@ -44,6 +120,23 @@ namespace FixedMath
         public static FFloat Acos(FFloat angle)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 将输入值钳制在指定范围内
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static FFloat Clamp(FFloat input, FFloat min, FFloat max)
+        {
+            if (input < min)
+                return min;
+            if (input > max)
+                return max;
+
+            return input;
         }
     }
 }
---
+++ FMath/Math/FMathTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FixedMath
+{
+    /// <summary>
+    /// 函数表
+    /// </summary>
+    public partial class FMathTable
+    {
+        /// <summary>
+        /// 所有表格值的缩放值（由生成表格数据时指定）
+        /// </summary>
+        public const int SCALE = 100000;
+    }
+}
